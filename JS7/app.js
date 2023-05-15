269aacc4ab202b871223c8eeba7299edd35e6a4c
ninivakh <120251081+ninivakh@users.noreply.github.com>
2023-05-15 21:00:08 +0300

    Add files via upload

---
+++ JS7/app.js
@@ -0,0 +1,11 @@
+var mas = [1, 2, -9, -56, 34, 77, -90, -11, -1, -2, -5, -9]
+
+var index = 0
+
+while (index < mas.length){
+    if(mas[index] < 0 ){
+        document.write(mas[index] + "<br>" )
+    }
+
+    index++
+}
